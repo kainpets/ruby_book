66b1b8d4b6ad68c2b2ef08cd7c12e49204c0d684
Pawel <pawelstepniak22@gmail.com>
2018-11-24 15:13:00 +0100

    adding ignore file

---
+++ new.rb
@@ -0,0 +1,8 @@
+name, adress, phone_num = ARGV
+
+puts "Your name is #{name}."
+name = $stdin.gets.chomp
+puts "Your adress is #{adress}."
+adress = $stdin.gets.chomp
+puts "Your phone number is #{phone_num.to_i}."
+phone = $stdin.gets.chomp
--- std12.rb
+++
@@ -1,5 +0,0 @@
-print "dej numer: , a obrażę Twoją matkę"
-numer = gets.chomp.to_f
-
-print "masz małego, daj drugi numer: "
-numer = gets.chomp.to_i
